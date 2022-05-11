4315622376186e4199a42fc2841062c9ece986bf
thompsian <thompsian@gmail.com>
2022-05-11 11:38:03 -0700

    Add location foreign key to item table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_11_182915) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_11_183424) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_11_182915) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "location_id"
+    t.index ["location_id"], name: "index_items_on_location_id"
   end
 
   create_table "locations", force: :cascade do |t|
@@ -27,4 +29,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_11_182915) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "items", "locations"
 end
