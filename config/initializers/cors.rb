0adb634d2d8a7b7b439001e48ff44562d51f9ef1
thompsian <thompsian@gmail.com>
2022-05-25 17:03:24 -0700

    CORS Test

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins ENV['app_address']
+      origins '*'
   
       resource '*',
         headers: :any,
